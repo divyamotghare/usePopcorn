2c4deede3f75036d6feafdd22673e72592520d3f
Divya Motghare <divyamotghare9@gmail.com>
2023-10-24 15:01:07 +0530

    added delete button in watched list

--- src/components/WatchedMovie.js
+++ src/components/WatchedMovie.js
@@ -1,4 +1,4 @@
-export default function WatchedMovie({ movie }) {
+export default function WatchedMovie({ movie, handleDeleteWatched }) {
   return (
     <li>
       <img src={movie.poster} alt={`${movie.title} poster`} />
@@ -16,6 +16,12 @@ export default function WatchedMovie({ movie }) {
           <span>⏳</span>
           <span>{movie.runtime} min</span>
         </p>
+        <button
+          className="btn-delete"
+          onClick={() => handleDeleteWatched(movie.imdbID)}
+        >
+          X
+        </button>
       </div>
     </li>
   );
